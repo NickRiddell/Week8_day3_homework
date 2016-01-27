b8528a3008b388582aa1137d17b7035efa00d8d8
Nick Riddell <nickjriddell@gmail.com>
2016-01-27 20:26:34 +0000

    Bank can testabley add a bank account...version 2

--- bank_functions_homework.js
+++ bank_functions_homework.js
@@ -2,8 +2,16 @@ var bank = {
   accounts: [],
   addAccount: function(account){
     this.accounts.push(account);
+    if (this.accounts != []) {
+      return true
+    };
+  },
+  findAccountHolder: function(name){
+    for (var i = 0; i < accounts.length; i++) {
+      var account = this.accounts[i]
+    };
+    return account;
   }
 }
-
 
 module.exports = bank
--- bank_homework_spec.js
+++ bank_homework_spec.js
@@ -2,10 +2,10 @@ var Bank = require('./bank_homework');
 var bank = require('./bank_functions_homework');
 var assert = require('assert');
 
-describe('account', function(){
-  it('should have the name jay', function(){
+describe('bank', function(){
+  it('should contain an account', function(){
     var account1 = new Bank('jay', 125.50, 'personal');
-    bank.addAccount();
-    assert.equal('jay', account1.name);
+    bank.addAccount(account1);
+    assert.equal(true, bank.addAccount());
   });
 });
